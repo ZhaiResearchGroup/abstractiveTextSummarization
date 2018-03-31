a9101344d05fd21c866d5d26acd9d6737c0985d0
Michael Colomb <colomb2@timan107.cs.illinois.edu>
2018-03-31 13:16:42 -0500

    encoder, decoder, attn

---
+++ src/model/attn.py
@@ -0,0 +1,17 @@
+class Attn(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(Attn, self).__init__()
+        
+        self.input_size = input_size
+        self.output_size = output_size
+
+        self.vec2energy = nn.Linear(self.input_size, output_size)
+        self.energy2out = nn.Linear(output_size, 1)
+
+    def forward(self, hidden, encoder_outputs):
+        return torch.bmm(encoder_outputs.transpose(0, 1), hidden.unsqueeze(2)).squeeze().unsqueeze(0)
+
+    def score(self, hidden, encoder_output):
+        energy = self.vec2energy(torch.cat((hidden, encoder_output), 1))
+        energy.data.test_name = "energies"
+        return self.energy2out(energy)
