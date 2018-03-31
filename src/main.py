a142cc5af82c968fb55a97d4c7b8d7b8970980be
Kabir Manghnani <kabirm2@timan107.cs.illinois.edu>
2018-03-30 22:00:26 -0500

    this prob isnt evryting

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import numpy as np
+from torch import cuda
+from utils.dataset import Dataset
+parser = argparse.ArgumentParser(description='train.py')
+## Data options
+parser.add_argument('-traindata', default='dataset/wiki_short.csv', help='Path to train data file')
+parser.add_argument("-testdata", default='dataset/wiki_short.csv',help="Path to the test data file")
+parser.add_argument("-processeddata", default='dataset/data.pkl',help="Path to the pre-processed data set")
+parser.add_argument("-sos",default="<sos>",help='Adding EOS token at the end of each sequence')
+
+parser.add_argument('-save_dir',default='saving', help='Directory to save model checkpoints')
+parser.add_argument('-load_from', type=str, help='Path to a model checkpoint')
+parser.add_argument("-vocab_size", type=int, default=None, help="Limit vocabulary")
+parser.add_argument('-gpu', type=int, default=-1,help='GPU id. Support single GPU only')
+opt = parser.parse_args()
+opt.cuda = (opt.gpu != -1)
+
+dataset = Dataset(opt, False)
---
+++ src/utils/dataset.py
@@ -0,0 +1,117 @@
+from torchtext.data import RawField
+from torchtext import data
+import torchtext.vocab as vocab
+from torchtext.data import RawField
+import csv
+import os
+import pickle
+import dill
+
+class Dataset(object):
+    def __init__(self, opt, eval):
+        self.cuda = opt.cuda
+       #self.batch_size = opt.batch_size
+        self.train_data = None 
+        self.sos_token = opt.sos
+        self.data_dir = opt.traindata
+        self.processed_data = opt.processeddata
+        self.load_data()
+        
+    def load_data(self):
+        if os.path.isfile(self.processed_data):
+            self.train_data = pickle.load(self.processed_data)
+            return
+        TEXT = data.Field(sequential=True, lower=True)
+        NUM = data.RawField()
+        SEN_VEC = data.RawField(postprocessing=self.sen_vec_postprocess)
+        SEN_IDX = data.RawField(postprocessing=self.sen_idx_postprocess)
+        # need to do this so we dont get errors about having too big of a file in a single cell of a csv
+        csv.field_size_limit(500 * 1024 * 1024)
+        self.train_data, = data.TabularDataset.splits( path='',train=self.data_dir, format='csv', 
+                                            skip_header = True, fields=[('query_num', NUM),
+                                                                        ('title', TEXT),
+                                                                        ('raw_query', TEXT),
+                                                                        ('sum', TEXT),
+                                                                        ('story', TEXT),
+                                                                        ('sen_vec', SEN_VEC),
+                                                                        ('sen_idx', SEN_IDX)])
+        with open(self.processed_data, 'wb') as f:
+            dill.dump(list(self.train_data), f)
+            #f.write('hi')
+    '''
+    @args:
+        batch: a list of strings (lenght batch_size) where each string is a document 
+            to be converted to sen_vec representation
+    @returns: 
+        torch.Variable that is (batch_size x num_sens_in_doc x 100) 
+                This array is the sentences embeddings for each sentence 
+                in each document in the batch
+    '''
+    @staticmethod
+    def sen_vec_postprocess(batch):
+
+        # split at <sos> tokens
+        tokenized_batch = [[self.sos_token + sen for sen in example.split(self.sos_token)][1:] for example in batch]
+        # tokenized batch is now a list[list[sentences]]
+
+        # maximum length of any document (in number of sentences)
+        max_doc_len = np.max(np.array([len(ex) for ex in tokenized_batch]))
+
+        batch_vec = np.zeros((len(batch), max_doc_len+1, 100))
+
+        for i, example in enumerate(tokenized_batch):
+            # length of this example tells us how much we need to leave as padding on the end
+            for j in range(len(example)):
+                batch_vec[i,:] = np.array(sent2vec.infer_vector(example[j]))
+
+        # return as cuda var
+        tensor = torch.FloatTensor(batch_vec)
+        if self.cuda:
+            tensor = tensor.cuda()
+        return torch.autograd.Variable(tensor)
+    
+    '''
+        @args:
+            batch: a list of strings where each string is a document for which 
+                we want to generate a mapping from word indcies to sentence indcies 
+                    (same input as sen_vec_postprocess)
+        @returns: 
+            torch.Variable that is (batch_size x num_sens_in_doc x 100) 
+                    This array is the sentences embeddings for each sentence 
+                    in each document in the batch
+    '''
+    @staticmethod
+    def sen_idx_postprocess(batch):
+        # split at <sos> tokens
+        sentences_batch = [[self.sos_token + sen for sen in example.split(self.sos_token)][1:] for example in batch]
+        # tokenized batch is now a list[list[sentences]]
+        max_num_sens = max([len(ex) for ex in sentences_batch])
+
+        # words batch is now a list[list[words]]
+        words_batch = [example.split() for example in batch]
+        # maximum length of any document (in number of words)
+        max_doc_len = max([len(ex) for ex in words_batch])
+
+
+        # initialize the array of sentence indices
+        # we use one more than the maximum to be the zero vector.
+        # In sentence attentions, this always receives zero weight
+        sen_idxs = np.ones((len(batch), max_doc_len)) * max_num_sens
+
+        # TODO: can someone check this and make sure it makes sense??
+        for sen_idx, example in enumerate(sentences_batch):
+            curr = 0
+            for wd_idx, sent in enumerate(example):
+                sen_idxs[sen_idx, curr:curr+len(sent.split())] = wd_idx
+                curr += len(sent.split())
+
+        if self.cuda:
+            return torch.autograd.Variable(torch.LongTensor(sen_idxs).cuda())
+        else:
+            return torch.autograd.Variable(torch.LongTensor(sen_idxs))
+
+if __name__ == '__main__':
+    dataset = Dataset()  # Default values are good
+    pdb.set_trace()
+    
+        
